f4e3ce3d726724a205acd004ce90c0476da5b1e5
Shubham Darade <138992223+ShubhamDarade@users.noreply.github.com>
2024-10-07 22:25:39 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 // Registration
 exports.register = async (req, res) => {
     const { name, email, password } = req.body;
-    const a = 100
+    const a = 1000
 
     try {
         const existingUser = await User.findOne({ email });
