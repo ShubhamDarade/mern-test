6eed48bf5f7af930bba1383fd7bff4dd054163ea
Shubham Darade <shubhamdarade717@gmail.com>
2024-10-09 00:14:19 +0530

    workflow run

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,10 +10,9 @@ exports.register = async (req, res) => {
     const d = 15
 
     //     Developers will need to create their own .env and .env.test: Since .env and .env.test files are ignored by Git, developers will need to create these files manually by copying the contents of .env.sample and .env.test.sample. The sample files will help them know what environment variables they need to define.
-
     // Typically, developers do this by running:
     //     cp .env.sample .env
-    // cp .env.test.sample .env.test
+    //     cp .env.test.sample .env.test
 
 
     try {
